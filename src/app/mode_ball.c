46c3cc63d388dd7eb6dd621e1e91062765a4c6a9
Gasem <al.mazharish.g@phystech.edu>
2026-05-17 23:38:58 +0000

    [fix] fix bag with pos < 0 in ball mode

--- src/app/mode_ball.c
+++ src/app/mode_ball.c
@@ -9,9 +9,9 @@
 #define BALL_BOUNCE_KEEP (0.8f)
 #define BALL_DT_SEC (1.0f / 8.0f)
 
-#define BALL_MIN_U (BALL_RADIUS_PX)
+#define BALL_MIN_U (BALL_RADIUS_PX + 1)
 #define BALL_MAX_U (DISPLAY_WIDTH - 1 - BALL_RADIUS_PX)
-#define BALL_MIN_V (BALL_RADIUS_PX)
+#define BALL_MIN_V (BALL_RADIUS_PX + 1)
 #define BALL_MAX_V (DISPLAY_HEIGHT - 1 - BALL_RADIUS_PX)
 
 typedef struct
@@ -24,7 +24,7 @@ typedef struct
 
 static ball_state_t s_ball;
 
-static void ball_reset(void)
+static void ball_reset()
 {
     s_ball.pos_u = (float)DISPLAY_CENTER_U;
     s_ball.pos_v = (float)DISPLAY_CENTER_V;
@@ -32,32 +32,38 @@ static void ball_reset(void)
     s_ball.vel_v = 0.0f;
 }
 
-static void integrate_axis(float *pos, float *vel,
-                           float accel, float min, float max)
+static void integrate_axis(float *pos, float *vel, float accel, float min, float max)
 {
     *vel += accel * BALL_DT_SEC;
     *vel *= (1.0f - BALL_FRICTION);
-    *pos += *vel * BALL_DT_SEC;
 
-    if (*pos < min)
+    float new_pos = *pos + *vel * BALL_DT_SEC;
+
+    if (new_pos < min)
     {
-        *pos = min;
+        new_pos = min;
         if (*vel < 0.0f)
         {
             *vel = -*vel * BALL_BOUNCE_KEEP;
         }
     }
-    else if (*pos > max)
+    else if (new_pos > max)
     {
-        *pos = max;
+        new_pos = max;
         if (*vel > 0.0f)
         {
             *vel = -*vel * BALL_BOUNCE_KEEP;
         }
     }
+
+    if (new_pos < min)
+        new_pos = min;
+    if (new_pos > max)
+        new_pos = max;
+    *pos = new_pos;
 }
 
-static void ball_enter(void)
+static void ball_enter()
 {
     ball_reset();
 }
@@ -80,8 +86,19 @@ static void ball_render(HAL_SSD1306_HandleTypeDef *display,
                           DISPLAY_WIDTH - 1, DISPLAY_HEIGHT - 1,
                           White);
 
+    int x = (int)(s_ball.pos_u + 0.5f);
+    int y = (int)(s_ball.pos_v + 0.5f);
+    if (x < BALL_MIN_U)
+        x = BALL_MIN_U;
+    if (y < BALL_MIN_V)
+        y = BALL_MIN_V;
+    if (x > BALL_MAX_U)
+        x = BALL_MAX_U;
+    if (y > BALL_MAX_V)
+        y = BALL_MAX_V;
+
     ssd1306_FillCircle(display,
-                       (uint8_t)s_ball.pos_u, (uint8_t)s_ball.pos_v,
+                       (uint8_t)x, (uint8_t)y,
                        BALL_RADIUS_PX, White);
 }
 
